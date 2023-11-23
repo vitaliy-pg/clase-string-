ba3a588adb57fd5be3349214f33e09d9156268e8
vitaliy-pg <aljndrvt@myuax.com>
2023-11-23 12:35:17 +0100

    declaro una subcadena

---
+++ cadenas clases string.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+
+int main() {
+
+    std::string miCadena = " los restaurantes japoneses no me gusatan";
+    std::string subcadenaBuscada = "ejemplo";
+    size_t indiceInicio = miCadena.find(subcadenaBuscada);
+
+
