1a00bea14e773c49123446fc4cfa85f51090ae81
vitaliy-pg <aljndrvt@myuax.com>
2023-11-23 12:44:54 +0100

    declaro las medidas

--- manipulacion avanzada de string.cpp
+++ manipulacion avanzada de string.cpp
@@ -5,3 +5,6 @@
 int main() {
 
     std::string miCadena = "Este es un ejemplo de C++ con strings.";
+    size_t indiceInicio = 11;
+    size_t longitudSubcadena = 7;
+    std::string subcadena = miCadena.substr(indiceInicio, longitudSubcadena);
