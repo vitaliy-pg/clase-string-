977b1d9765cc78b70b7cb186c76d7f05defad4f1
vitaliy-pg <aljndrvt@myuax.com>
2023-11-23 12:29:48 +0100

    añado mas cadenas

--- c++.cpp
+++ c++.cpp
@@ -4,7 +4,10 @@
 int main() {
 
     std::string cadena1 = "Hola, ";
-    std::string cadena2 = "mundo!";
+    std::string cadena2 = "Ruben.
+    std::string cadena3 = "! ¿Cómo estás?";
+    std::string cadena4 = " Espero que todo vaya bien.";
+    std::string cadena5 = " ¡Hasta luego!";
 
 
     std::string resultado = cadena1 + cadena2;
