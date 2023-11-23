f91f7b708986165c3bef0dda0e71e631011b197b
vitaliy-pg <aljndrvt@myuax.com>
2023-11-23 12:23:22 +0100

    declaro el tamaño de la cadena y si esta vacia o llena

--- funciones string.cpp
+++ funciones string.cpp
@@ -8,3 +8,12 @@ int main() {
     // Leer una cadena desde la entrada estándar
     std::cout << "Ingrese una cadena: ";
     std::getline(std::cin, miCadena);
+
+    std::cout << "El tamaño de la cadena es: " << miCadena.size() << " caracteres." << std::endl;
+
+    // Comprobar si la cadena está vacía utilizando la función empty()
+    if (miCadena.empty()) {
+        std::cout << "La cadena está vacía." << std::endl;
+    } else {
+        std::cout << "La cadena NO está vacía." << std::endl;
+    }
