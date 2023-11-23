56b3442bfb5a31d587ac7358125e95ab2e921ae7
vitaliy-pg <aljndrvt@myuax.com>
2023-11-23 12:19:48 +0100

    funcion string para crear mensajes

---
+++ cadenaSTL.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+    std::string frase = "Hola, soy vitaliy";
+    std::string frase2 = "Ruben me pone un cero";
+    std::string frase3 = frase + " " + frase2;
+
+
+    std::cout << "Frase: " << frase << std::endl;
+    std::cout << "Frase2: " << frase2 << std::endl;
+    std::cout << "Frase3: " << frase3 << std::endl;
+}
