a16142925c4ab1e6fdba46c5a1c5a21e8ee32121
vitaliy-pg <aljndrvt@myuax.com>
2023-11-23 12:22:13 +0100

    hago una cadena de caracteres

--- funciones string.cpp
+++ funciones string.cpp
@@ -1,2 +1,10 @@
 #include <iostream>
 #include <string>
+
+int main() {
+    // Crear una cadena utilizando la clase string
+    std::string miCadena;
+
+    // Leer una cadena desde la entrada estándar
+    std::cout << "Ingrese una cadena: ";
+    std::getline(std::cin, miCadena);
