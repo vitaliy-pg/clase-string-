7cd43cc0da5d715f94f4dc050dd53c12aaeba6e8
vitaliy-pg <aljndrvt@myuax.com>
2023-11-23 12:24:15 +0100

    imprimo la cadena

--- funciones string.cpp
+++ funciones string.cpp
@@ -2,18 +2,25 @@
 #include <string>
 
 int main() {
-    // Crear una cadena utilizando la clase string
+
     std::string miCadena;
 
-    // Leer una cadena desde la entrada estándar
     std::cout << "Ingrese una cadena: ";
     std::getline(std::cin, miCadena);
 
     std::cout << "El tamaño de la cadena es: " << miCadena.size() << " caracteres." << std::endl;
 
-    // Comprobar si la cadena está vacía utilizando la función empty()
+
     if (miCadena.empty()) {
         std::cout << "La cadena está vacía." << std::endl;
     } else {
         std::cout << "La cadena NO está vacía." << std::endl;
     }
+    std::string segundaCadena = " Esta es otra cadena.";
+    miCadena += segundaCadena;
+
+
+    std::cout << "Cadena después de la concatenación: " << miCadena << std::endl;
+
+    return 0;
+}
