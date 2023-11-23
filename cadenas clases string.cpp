47ed04638b43717633ba1ee4e1f818666cd11c25
vitaliy-pg <aljndrvt@myuax.com>
2023-11-23 12:36:28 +0100

    declaro dos subcadenas

--- cadenas clases string.cpp
+++ cadenas clases string.cpp
@@ -7,4 +7,10 @@ int main() {
     std::string subcadenaBuscada = "ejemplo";
     size_t indiceInicio = miCadena.find(subcadenaBuscada);
 
+    if (indiceInicio != std::string::npos) {
+        std::cout << "La subcadena '" << subcadenaBuscada << "' comienza en el índice: " << indiceInicio << std::endl;
+    } else {
+        std::cout << "La subcadena '" << subcadenaBuscada << "' no se encuentra en la cadena." << std::endl;
+    }
+
 
