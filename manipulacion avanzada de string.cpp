6d8799675491bdec0ecfea1b58b5b20d1cc4a866
vitaliy-pg <aljndrvt@myuax.com>
2023-11-23 12:50:01 +0100

    mejoro mi cadena

--- manipulacion avanzada de string.cpp
+++ manipulacion avanzada de string.cpp
@@ -5,6 +5,9 @@
 int main() {
 
     std::string miCadena = "Este es un ejemplo de C++ con strings.";
+
+    std::cout << "Cadena original: " << miCadena << std::endl;
+
     size_t indiceInicio = 11;
     size_t longitudSubcadena = 7;
     std::string subcadena = miCadena.substr(indiceInicio, longitudSubcadena);
@@ -15,7 +18,14 @@ int main() {
     std::string cadenaAInsertar = "manipulación ";
     miCadena.insert(indiceInsercion, cadenaAInsertar);
 
-    std::cout << "Cadena resultante después de la inserción: " << miCadena << std::endl;
+    std::cout << "Cadena después de la inserción: " << miCadena << std::endl;
+
+    size_t indiceEliminacion = 11;
+    size_t longitudEliminacion = 7;
+    miCadena.erase(indiceEliminacion, longitudEliminacion);
+
+
+    std::cout << "Cadena después de la eliminación: " << miCadena << std::endl;
 
     return 0;
 }
